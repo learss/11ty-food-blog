a247b37e0488487f9a259a021fe1fe0daf4165bc
kingslylear <84743678+kingslylear@users.noreply.github.com>
2022-07-18 18:12:59 +0100

    Setup CMS

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
 	// Return your Object options:
 	eleventyConfig.addPassthroughCopy("./src/style.css");
 	eleventyConfig.addPassthroughCopy("./src/assets");
+	eleventyConfig.addPassthroughCopy("./src/admin");
 
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
